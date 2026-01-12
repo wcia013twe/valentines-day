38f7b0a5c09f849077d8d83c1d640871ba3e50cc
Devowski <devowski256@gmail.com>
2026-01-12 02:49:40 +0100

    sth

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const LANES = 3;
-const JUMP_SPEED = -450;
-const GRAVITY = 800;
-const X_SPEED = 500;
-const PROGRESS_SPEED = 40;
+const JUMP_SPEED = -700;
+const GRAVITY = 2500;
+const X_SPEED = 1000;
+const Z_SPEED = 100;
 const INIT_LANE = 1;
-const LANE_WIDTH = 142;
+const LANE_WIDTH = 46;
 const SPAWN_Z = -1000;
 
+const ANGLE = (30 * Math.PI) / 180;
+const PERSPECTIVE = 600;
+const WIDTH = 500;
+const HEIGHT = 600;
+const ROAD_HEIGHT = 2500;
+
 const view = {
-  scene: document.getElementById('scene'),
+  game: document.getElementById('game'),
   road: document.getElementById('road'),
   runner: document.getElementById('runner'),
   cubeBlueprint: document.getElementById('cube-blueprint'),
@@ -18,24 +24,77 @@ const view = {
 const state = {
   progress: 0,
   runner: {
-    position: { x: getLaneX(INIT_LANE), y: 0 },
+    position: { x: getLaneX(INIT_LANE), y: 0, z: -420 },
     lane: INIT_LANE,
     ySpeed: 0,
   },
   objects: [],
 };
 
-function createFromBlueprint(blueprint) {
-  const cube = blueprint.cloneNode(true);
-  cube.removeAttribute('id');
-  road.append(cube);
+function createFromBlueprint(blueprint, position) {
+  const element = blueprint.cloneNode(true);
+  element.removeAttribute('id');
+  view.game.append(element);
 
-  return cube;
+  return element;
+}
+
+function perspectiveScale(z) {
+  return PERSPECTIVE / (PERSPECTIVE + z);
+}
+
+function project({ x, y, z }, rescale = 1) {
+  const rY = y * Math.cos(ANGLE) - z * Math.sin(ANGLE);
+  const rZ = y * Math.sin(ANGLE) + z * Math.cos(ANGLE);
+
+  const scale = perspectiveScale(rZ);
+
+  return {
+    x: x * scale,
+    y: rY * scale,
+    z: rZ,
+    scale: rescale * scale,
+  };
+}
+
+function getTransform({ x, y, scale }) {
+  return `translateX(${x}px) translateY(${y}px) scale(${scale})`;
 }
 
 function getLaneX(lane) {
   return (lane - 1) * LANE_WIDTH;
 }
+
+function spawnObjects() {
+  let lane = 0;
+
+  for (let i = 0; i < 20; i++) {
+    const x = getLaneX(lane);
+    const y = 0;
+    const z = 300 - i * 50;
+
+    const position = { x, y, z };
+
+    const object = {
+      element: createFromBlueprint(view.coinBlueprint, position),
+      position,
+    };
+
+    if (Math.random() > 0.8) {
+      const cubePosition = { ...position };
+      state.objects.push({
+        element: createFromBlueprint(view.cubeBlueprint, cubePosition),
+        position: cubePosition,
+      });
+
+      position.y -= 50;
+    }
+
+    state.objects.push(object);
+  }
+}
+
+spawnObjects();
 
 document.addEventListener('keydown', (event) => {
   const { runner } = state;
@@ -44,9 +103,11 @@ document.addEventListener('keydown', (event) => {
     case 'ArrowLeft':
       runner.lane = Math.max(runner.lane - 1, 0);
       return;
+
     case 'ArrowRight':
       runner.lane = Math.min(runner.lane + 1, LANES - 1);
       return;
+
     case 'ArrowUp':
       if (runner.position.y === 0) {
         runner.ySpeed = JUMP_SPEED;
@@ -55,7 +116,7 @@ document.addEventListener('keydown', (event) => {
 });
 
 function update(dt) {
-  const { runner } = state;
+  const { runner, objects } = state;
 
   const { position } = runner;
   const destinationX = getLaneX(runner.lane);
@@ -71,19 +132,30 @@ function update(dt) {
   }
   position.y = Math.min(position.y + runner.ySpeed * dt, 0);
 
-  const increment = PROGRESS_SPEED * dt;
-  state.progress += increment;
+  const zSpeed = Z_SPEED * dt;
+  state.progress += zSpeed / Math.cos(ANGLE);
+  for (const object of objects) {
+    object.position.z -= zSpeed;
+  }
 }
 
 function draw() {
-  const { progress, runner } = state;
-  const { position } = runner;
+  const { runner, objects, progress } = state;
 
   view.road.style.backgroundPositionY = `${progress}px`;
 
-  view.runner.style.transform = `translateX(${position.x}px) translateY(${position.y}px) translateZ(-30px) scale(0.5)`;
+  const runnerPosition = project(runner.position, 0.2);
+  view.runner.style.zIndex = Math.floor(-runnerPosition.z);
+  view.runner.style.transform = getTransform(runnerPosition);
   view.runner.style.animationPlayState =
     runner.position.y < 0 ? 'paused' : 'running';
+
+  for (const object of objects) {
+    const position = project(object.position);
+
+    object.element.style.zIndex = Math.floor(-position.z);
+    object.element.style.transform = getTransform(position);
+  }
 }
 
 let prevTime = performance.now();
